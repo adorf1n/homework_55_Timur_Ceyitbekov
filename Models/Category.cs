ee8514e8e557f51be0a7179c4bd1bd468bf8c0c0
ad0rfin <adorfin@list.ru>
2024-10-28 18:13:50 +0600

    второе задание выполнено

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,12 @@
-﻿public class Category
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Category
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Введите название категории.")]
+    [StringLength(100, ErrorMessage = "Название категории не должно превышать 100 символов.")]
     public string Name { get; set; }
 
-    public ICollection<Product> Products { get; set; } = new List<Product>(); 
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,26 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 public class Product
 {
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Введите название товара.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Название товара должно содержать от 3 до 100 символов.")]
     public string Name { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Введите стоимость товара.")]
+    [Range(50, double.MaxValue, ErrorMessage = "Стоимость товара должна быть не менее 50.")]
     public decimal Price { get; set; }
 
     public string? ImagePath { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Выберите категорию.")]
     public int CategoryId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Выберите бренд.")]
     public int BrandId { get; set; }
 
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    // Навигационные свойства
     public virtual Category? Category { get; set; }
     public virtual Brand? Brand { get; set; }
-
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
